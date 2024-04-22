16e0f7a76863939a24f2b71d9ce6bbadf7fcd13f
Tomas Godoybaca <tommygodoybaca@outlook.com>
2024-04-22 11:08:36 -0600

    styling and content changes made -removed logo from main page -added filler cards to art, linked filler cards to collapseable content -added scripts to control the cards -style changes -added media querys -styled the cards -added sass variables for breakpoints

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 $('#mobile-nav').on('click', function () {
         $('#navbar').slideToggle();
 })
+
+$('#card-1').on('click', function () {
+        $('#card-content-1').slideToggle("slow");
+})
+
+$('#card-2').on('click', function () {
+        $('#card-content-2').slideToggle("slow");
+})
+
+$('#card-3').on('click', function () {
+        $('#card-content-3').slideToggle("slow");
+})
