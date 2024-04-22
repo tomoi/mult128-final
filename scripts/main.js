4db8c7617b72476fc8bf77eb41caf8c8365be5fb
Tomas Godoybaca <tommygodoybaca@outlook.com>
2024-04-22 11:46:19 -0600

    Minor html and styling changes -added 'read more' 'buttons' to cards so people know to click -changed js to change the 'read more' to 'scroll' so people know to scroll after they click -added breakpoints for the cards -changed the cursor on hover so users know they can click

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,15 @@ $('#mobile-nav').on('click', function () {
 
 $('#card-1').on('click', function () {
         $('#card-content-1').slideToggle("slow");
+        $('#card-1 > h4').text('Scroll');
 })
 
 $('#card-2').on('click', function () {
         $('#card-content-2').slideToggle("slow");
+        $('#card-2 > h4').text('Scroll');
 })
 
 $('#card-3').on('click', function () {
         $('#card-content-3').slideToggle("slow");
+        $('#card-3 > h4').text('Scroll');
 })
