d1003125ddc6a80eaea01e8c6891061823271e9f
Tomas Godoybaca <tommygodoybaca@outlook.com>
2024-04-22 13:16:25 -0600

    changed js, scss, and html -added arrows to prompt the user to scroll down -started styling the arrow

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,17 @@ $('#mobile-nav').on('click', function () {
 $('#card-1').on('click', function () {
         $('#card-content-1').slideToggle("slow");
         $('#card-1 > h4').text('Scroll');
+        $('#card-1 > h4').addClass('card-arrow');
 })
 
 $('#card-2').on('click', function () {
         $('#card-content-2').slideToggle("slow");
         $('#card-2 > h4').text('Scroll');
+        $('#card-2 > h4').addClass('card-arrow');
 })
 
 $('#card-3').on('click', function () {
         $('#card-content-3').slideToggle("slow");
         $('#card-3 > h4').text('Scroll');
+        $('#card-3 > h4').addClass('card-arrow');
 })
