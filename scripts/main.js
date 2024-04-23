9aa552b4749e1d0b52ce7a029fd68c599c669e9e
Tomas Godoybaca <tommygodoybaca@outlook.com>
2024-04-23 10:36:02 -0600

    Added photos to photography page -added responsive photos -edited javascript fo rart page to close all other card content when opening new ones -added styling for photography page -added filter for business link logos to make them black to fit with the theme

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,26 @@ $('#card-1').on('click', function () {
         $('#card-content-1').slideToggle("slow");
         $('#card-1 > h4').text('Scroll');
         $('#card-1 > h4').addClass('card-arrow');
+
+        $('#card-content-3').slideUp("slow");
+        $('#card-content-2').slideUp("slow");
 })
 
 $('#card-2').on('click', function () {
         $('#card-content-2').slideToggle("slow");
         $('#card-2 > h4').text('Scroll');
         $('#card-2 > h4').addClass('card-arrow');
+
+        $('#card-content-3').slideUp("slow");
+        $('#card-content-1').slideUp("slow");
+
 })
 
 $('#card-3').on('click', function () {
         $('#card-content-3').slideToggle("slow");
         $('#card-3 > h4').text('Scroll');
         $('#card-3 > h4').addClass('card-arrow');
+
+        $('#card-content-2').slideUp("slow");
+        $('#card-content-1').slideUp("slow");
 })
